eac2814bbac34e938a641c1fc3ef20685f2380f9
youngprinnce <ajiboyeadedotun16@gmail.com>
2023-05-03 06:06:35 +0100

    middlwares to handle error, 404, timedout conn

---
+++ src/api/middlewares/index.ts
@@ -0,0 +1,7 @@
+import timedout from './timedout';
+import errorHandler from './error';
+import rateLimiter from './rate-limiter';
+import badJsonHandler from './validate-json';
+import notFoundHandler from './not-found-error';
+
+export {errorHandler, notFoundHandler, badJsonHandler, timedout, rateLimiter}
