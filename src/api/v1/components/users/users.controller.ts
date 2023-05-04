f3ed27a415dec4d29cce824f5b5eb818834bb327
youngprinnce <ajiboyeadedotun16@gmail.com>
2023-05-04 04:31:16 +0100

    cache all users

--- src/api/v1/components/users/users.controller.ts
+++ src/api/v1/components/users/users.controller.ts
@@ -1,11 +1,18 @@
 import userService from "./users.service";
 import { Request, Response, NextFunction } from 'express';
+import { cacheData, getCache } from "../../../../utils/helpers";
 
 export = {
   async users(req:Request, res:Response, next:NextFunction) {
     let {currentPage, limit} = req.query;
     try {
+      const cachedUsers = await getCache({req});
+      if(cachedUsers) {
+        const {status, ...rest} = cachedUsers
+        return res.status(status).json(rest);
+      }
       const {status, ...rest} = await userService.users({currentPage, limit});
+      await cacheData({req, data: {status, ...rest}})
       return res.status(status).json(rest);
     } catch (err) {
       return next(err);
